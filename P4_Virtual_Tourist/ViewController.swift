7bbdef2bba6ad1095d603af5f189c5e8a30b200d
Matt Sommer <matthew.sommer@gmail.com>
2015-11-29 17:11:10 -0800

    Initial commit of default Xcode project.

---
+++ P4_Virtual_Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  P4_Virtual_Tourist
+//
+//  Created by Matt Sommer on 11/29/15.
+//  Copyright © 2015 Matt Sommer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
